f73408d7fe2bf1d15e9a80248757db32d939d845
Honorius di Gesus <honoriusdigesus@gmail.com>
2024-10-16 22:11:38 -0500

    Configuración de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component'),
+    children:[
+      {
+        path: 'product',
+        title: 'Products',
+        loadComponent: () => import('./dashboard/pages/product/product.component')
+      },
+      {
+        path:'user',
+        title: 'Users',
+        loadComponent: () => import('./dashboard/pages/user/user.component')
+      },
+      {
+        path:'role',
+        title: 'Roles',
+        loadComponent: () => import('./dashboard/pages/role/role.component')
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  }
+];
