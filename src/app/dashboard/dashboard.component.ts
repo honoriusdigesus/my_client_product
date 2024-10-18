84cbc2c3cc0efcaf85c903da7da05ebf9e21033f
Honorius di Gesus <honoriusdigesus@gmail.com>
2024-10-17 23:53:14 -0500

    Configuración de rutas

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {SideMenuComponent} from './shared/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    SideMenuComponent
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
