223b609bd711f75c381902703d1e9652793f2aba
Honorius di Gesus <honoriusdigesus@gmail.com>
2024-10-18 19:34:27 -0500

    Dashboard personalizada

--- src/app/dashboard/shared/side-menu/side-menu.component.ts
+++ src/app/dashboard/shared/side-menu/side-menu.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {routes} from '../../../app.routes';
+import {RouterLink, RouterLinkActive} from '@angular/router';
 
 @Component({
   selector: 'app-side-menu',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
 
+  public menuItems = routes.map((route)=>route.children ?? []).flat()
 }
